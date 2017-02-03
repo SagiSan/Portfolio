3f63c0ae07cd5f77d009eebecd7c375fe26b5e2b
SagiSan <samir.sagi@gmail.com>
2017-02-03 16:12:02 +0100

    scroll-speed

--- scripts.js
+++ scripts.js
@@ -18,5 +18,22 @@
         });
     });
 
+
+// scroll speed
+
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 });
   }(jQuery));
