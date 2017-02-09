6a0fcc3f6f12f5b655144f726dbb2561d467343a
SagiSan <samir.sagi@gmail.com>
2017-02-09 13:53:10 +0100

    mail-update

--- scripts.js
+++ scripts.js
@@ -1,50 +1,56 @@
 
-   function mail() {
-        window.open('mailto:samir.sagi@gmail.com?subject=User&body=message');
-
-    }
-
+function sendMail() {
+    $("mailform").ajaxSubmit({ url: 'http://solution404.cloudapp.net:1337/send/send', type: 'POST' });
+}
 
 (function ($) {
-  $(document).ready(function(){
+    $(document).ready(function () {
 
-    // hide .navbar first
-    $(".navbar").hide();
 
-    // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 100) {
-                $('.navbar').fadeIn();
-            } else {
-                $('.navbar').fadeOut();
-            }
+        $('#mailform').submit(function () {
+            $("#mailform").ajaxSubmit({ url: 'http://solution404.cloudapp.net:1337/send/send', type: 'POST' });
+            $('#mailform')[0].reset();
+            alert("Mail has been sent successfully");
+            return false;
         });
+
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 100) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+
+
+        // scroll speed
+
+
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+
+            var target = this.hash;
+            var $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+
+
+        //  mail
+
+
+
     });
-
-
-// scroll speed
-
-
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-
-
-//  mail
-
- 
-
-});
-  }(jQuery));
+} (jQuery));
